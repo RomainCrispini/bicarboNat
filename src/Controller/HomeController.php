c141d1cf30546ef7d9e38a7d36daeb5bf755e680
RomainCrispini <romain.crispini@gmail.com>
2024-01-06 20:08:18 +0100

    Init2

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,9 +8,9 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/', name: 'app_home', methods: ['GET', 'POST'])]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('pages/home.html.twig');
     }
 }
